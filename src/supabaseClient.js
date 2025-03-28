4866326406e27952ac996710d9900ba0bad9f287
pierrequiroul <pierrelac@hotmail.be>
2025-03-28 16:06:12 +0100

    js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,6 +4,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  process.env.SUPABASE_URL || '',
+  process.env.SUPABASE_SERVICE_ROLE_KEY || ''
 );
